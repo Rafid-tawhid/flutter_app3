a5c67a19c97dd1b78ec1c4a103105ba9ec750e3e
Tawhidur Rahman Rafid <ilovemymom12345>
2021-04-29 11:56:48 +0600

    Container basics

--- lib/main.dart
+++ lib/main.dart
@@ -3,61 +3,31 @@ import 'package:flutter/material.dart';
 
 
 void main() {
-  runApp(MyApp());
+  runApp(registration());
 }
 
-class MyApp extends StatelessWidget {
+class registration extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "TexBox & Dropdown",
-      home: book(),
-    );
-    
-  }
-}
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            height: 200,
+            width: 350,
+            decoration: BoxDecoration(color: Colors.deepOrange),
+            child: Center(
+              child: Text(
+                "I love coding",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.deepPurple,
 
-class book extends StatefulWidget {
-  @override
-  _bookState createState() => _bookState();
-}
-
-class _bookState extends State<book> {
-  String namebook="";
-  var writers=["Rafid Tawhid","Hymayun","kobir","salman"];
-  var _currentItem='Rafid Tawhid';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Stateful Widget"),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextField(
-              onChanged: (String userinput)
-              {
-                setState(() {
-                  namebook=userinput;
-                });
-              },
+                ),
+              ),
             ),
-
-             DropdownButton(items: writers.map((String dropdownitems){
-               return DropdownMenuItem(value: dropdownitems,child: Text(dropdownitems),);
-
-             }).toList(),
-               onChanged: (String newVal)
-               {
-                 setState(() {
-                   this._currentItem=newVal;
-                 });
-               },
-               value: _currentItem,),
-            Text("Enter book name is $namebook",style: TextStyle(fontSize: 20),)
-          ],
+          ),
         ),
       ),
     );
