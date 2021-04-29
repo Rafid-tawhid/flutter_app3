ec1190cd2634472ffe1d37c3525dfd3a824f2ab0
Tawhidur Rahman Rafid <ilovemymom12345>
2021-04-29 12:14:59 +0600

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -14,54 +14,56 @@ class registration extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
        appBar: AppBar(title: Text("Row & Column"),),
-          body: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.deepPurple,
-                ),
-                SizedBox(width: 10,),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                SizedBox(width: 10,),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                SizedBox(width: 10,),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.blueAccent,
-                ),
-                SizedBox(width: 10,),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.green,
-                ),
-                SizedBox(width: 10,),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.deepPurpleAccent,
-                ),
-                SizedBox(width: 10,),
+          body: ListView(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.deepPurple,
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.blueAccent,
+                  ),
+                  SizedBox(
+                      height: 10,),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.green,
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  SizedBox(height: 10,),
 
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
